a52b65542533e8ed56257781026b6925e112f10e
cadirnberger <cadirnberger@willamette.edu>
2025-04-28 09:54:43 -0700

    Create UI.php

---
+++ UI.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>UI</h1>
+    <p>hi this is my web site</p>
+    <py-script src="simlog.py"></py-script>
+    
+</body>
+</html>
